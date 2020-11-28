9bcbd8ff3ea8b2bc3a275e0beb692993583fa1fc
Carolina Rosemback <carolinarosemback@gmail.com>
2020-11-28 20:19:02 +0000

    Did game_over and doRound

--- TP1Final/src/game.pl
+++ TP1Final/src/game.pl
@@ -2,11 +2,32 @@
 startGame(Player1, Player2) :-
 	initial(InitialBoard),
 	gameState(Player1, InitialBoard, GameState),
-	doRound(GameState, Player1, Player2, Player1).
+	doRound(GameState).
 	
-%doRound(+GameState, +Player1, +Player2, +CurrentTurn)
-doRound(GameState, Player1, Player2, CurrentTurn).
-	
+
+
+next_turn('Player1', 'Player2').
+next_turn('Player2', 'Player1').
+
+changeTurn([H|T]):-
+	next_turn(H, NewPlayer),
+	GameState = [NewPlayer|T],
+	gameState(NewPlayer, T, GameState).
+
+
+%doRound(+GameState)
+doRound(GameState):-
+	%condição de paragem 
+	game_over(GameState, Winner).
+
+%doRound(+GameState)
+doRound(GameState):-
+	% \+condição de paragem 
+	format('Player ~w turn', [H|_]),
+	move(GameState, Move, NewGameState),
+	changeTurn(GameState),
+	doRound(GameState).
+
 	
 %valid_moves(+GameState, +Player, -ListOfMoves)
 valid_moves(GameState, Player, ListOfMoves).
@@ -14,8 +35,22 @@ valid_moves(GameState, Player, ListOfMoves).
 %move(+GameState, +Move, -NewGameState)​
 move(GameState, Move, NewGameState).
 
+ 
+%TODO criar caso de desempate
+%check for the winner
+check_winner(GameState, Winner):-
+	value(GameState, Player1, Val1),
+	value(GameState, Player2, Val2),
+		Val1 @< Val2,
+		Winner = 'Player1';
+        Val2 @< Val1,
+		Winner = 'Player2'.
+
 %game_over(+GameState, -Winner)
-game_over(GameState, Winner).
+game_over(GameState, Winner):-
+	write('Game Over!\n'),
+    checkWinner(GameState,Winner),
+	format('\n ~w is the winner!', Winner).
 
 %value(+GameState, +Player, -Value)​
 value(GameState, Player, Value).
