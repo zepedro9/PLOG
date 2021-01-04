fc4715066c073d2bb7286b00e880eea2bc224a2e
Zé Pedro <nepsname@gmail.com>
2021-01-04 14:43:38 +0000

    Update dominosweeper.pl

--- TP2/dominosweeper.pl
+++ TP2/dominosweeper.pl
@@ -2,12 +2,23 @@
 :- use_module(library(lists)).
 :- use_module(library(random)).
 
+% Tabuleiro 6vs6
 % A1, A2, A3, A4, A5, A6
 % B1, B2, B3, B4, B5, B6
 % C1, C2, C3, C4, C5, C6
 % D1, D2, D3, D4, D5, D6
 % E1, E2, E3, E4, E5, E6
 % F1, F2, F3, F4, F5, F6
+
+% Cell values:
+% -1 = Mine
+% 0 = 0
+% 1 = 1
+% 2 = 2
+% 3 = 3
+% 4 = 4
+%
+% Obs: With the given restrictions, a higher value than 4 in any cell is impossible.
 
 dominosweeper(Vars) :-
 	Vars = [A1, A2, A3, A4, A5, A6, B1, B2, B3, B4, B5, B6, C1, C2, C3, C4, C5, C6, D1, D2, D3, D4, D5, D6, E1, E2, E3, E4, E5, E6, F1, F2, F3, F4, F5, F6],
